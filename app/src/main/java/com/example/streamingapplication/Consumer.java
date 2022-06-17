6e047e0d2ad8a3b78f67132a82bbc0214b8a87ae
Michael Papapetros <michalispap99@github.com>
2022-06-18 01:03:08 +0300

    Update IPs

--- app/src/main/java/com/example/streamingapplication/Consumer.java
+++ app/src/main/java/com/example/streamingapplication/Consumer.java
@@ -8,7 +8,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -31,7 +30,7 @@ public class Consumer implements Parcelable {
 
     protected ArrayList<Address> brokers = new ArrayList<>(Arrays.asList(
             /// first random broker IP and Port
-            new Address("192.168.1.5", 6000)
+            new Address("192.168.1.7", 6000)
     ));
 
     public Consumer(Address _addr){
